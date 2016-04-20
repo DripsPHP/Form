700e3a5281feaac84d7d38e8f4a2339adbbde99e
Raffael Kessler <raffael.kessler@prowect.com>
2016-04-20 20:39:14 +0200

    Grundstruktur

--- src/Form.php
+++ src/Form.php
@@ -4,5 +4,23 @@ namespace Drips\Form;
 
 class Form
 {
+    public function __construct()
+    {
 
+    }
+
+    public function isSubmitted()
+    {
+
+    }
+
+    public function isValid()
+    {
+
+    }
+
+    public function bind()
+    {
+        
+    }
 }
